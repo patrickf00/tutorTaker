b2214af94315bc04524e65e496796cd90bd29203
kela4560 <55550467+kela4560@users.noreply.github.com>
2019-10-23 12:11:58 -0600

    Add files via upload

--- Driver.cpp
+++ Driver.cpp
@@ -8,9 +8,9 @@ int main(){
   Graph basicTest;
   string subject[10] = {"Calculus", "Chemistry", "Biology"};
   vector<double> v1{5, 4.6, 4.3};
-  basicTest.addVertex("Joe Courington", 1, subject, 3, v1);//tutor
-  basicTest.addVertex("Christian Molina", 0, subject, 2, v1);//non tutor
-  basicTest.addEdge("Joe Courington","Christian Molina");//tutors will be connected to students only in one way, do not need double connection
+  basicTest.addVertex("Joe Courington", 1, "CU Boulder", subject, 3, v1);//tutor
+  basicTest.addVertex("Christian Molina", 0, "CU Boulder", subject, 2, v1);//non tutor
+//tutors will be connected to students only in one way, do not need double connection
   basicTest.displayEdges();
   basicTest.displayNames();
 }
