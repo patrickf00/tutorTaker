663f2f8de3426eabb89e7aa18343e86b50522d5e
patrick-faulkner <45801641+patrick-faulkner@users.noreply.github.com>
2019-11-27 15:11:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(express.static(__dirname + '/')); // This line is necessary for us to us
 
 // will render index page (renders as login)
 app.get('/', function(req, res){
-	res.render('pages/LoginPage',{})
+	res.redirect('/login',{})
 });
 
 /* will render the search page*/
@@ -101,27 +101,27 @@ app.get('/tutor-finder/filter', function(req, res){
 });
 
 // //will render base login page
-// app.get('/login', function(req, res){
-//   var query1 = 'SELECT username,pwdHash FROM Users;';
-//   db.query(query1, task => {
-//       return task.batch([
-//           task.any(query1)
-//       ]);
-//   })
-//   .then(data => {
-//     console.log(data)
-//     res.render('pages/LoginPage',{
-//         users: data
-//       })
-//   })
-//   .catch(err => {
-//       // display error message in case an error
-//       console.log('error', err);
-//       res.render('pages/LoginPage',{
-//            users: ''
-//       })
-//   })
-// });
+app.get('/login', function(req, res){
+  res.render('pages/loginPage');
+  db.query(query1, task => {
+      return task.batch([
+          task.any(query1)
+      ]);
+  })
+  .then(data => {
+    console.log(data)
+    res.render('pages/LoginPage',{
+        users: data
+      })
+  })
+  .catch(err => {
+      // display error message in case an error
+      console.log('error', err);
+      res.render('pages/LoginPage',{
+           users: ''
+      })
+  })
+});
 
 //will get request for verification process the login page
 app.post('/login/verify', function(req, res){
