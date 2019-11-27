9a3e2c0520d2fadf8444f9816ab18eb381aea488
patrick-faulkner <45801641+patrick-faulkner@users.noreply.github.com>
2019-11-27 09:14:52 -0700

    added query for feedback

--- server.js
+++ server.js
@@ -184,6 +184,8 @@ app.get('/profile', function(req, res){
     });
   }else{
     var query1 = "SELECT * FROM users WHERE id='" + req.session.uid + "';";
+    // gets all feed back for user
+    var query2 = "SELECT reviewText FROM feedback WHERE userID= '" + req.session.uid "';";
     console.log(query1)
     db.query(query1, task => {
       return task.batch([
