a1083d26770899d9d4ba762104a7d161e1b57802
patrick-faulkner <45801641+patrick-faulkner@users.noreply.github.com>
2019-11-27 17:05:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -167,12 +167,12 @@ app.get('/profile', function(req, res){
   }else{
     var query1 = "SELECT * FROM users WHERE id='" + req.session.uid + "';";
     // gets all feed back for user
-    var query2 = "SELECT reviewText FROM feedback WHERE userID= '" + req.session.uid + "';";
+    //var query2 = "SELECT reviewText FROM feedback WHERE userID= '" + req.session.uid + "';";
     console.log(query1)
     db.query('get-everything', task => {
       return task.batch([
-        task.any(query1),
-        task.any(query2)
+        task.any(query1)
+        //task.any(query2)
       ]);
     })
     .then(data => {
