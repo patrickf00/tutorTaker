17b12c2ac51f5632a3a2f94c805af4d4a3bcbf1f
patrick-faulkner <45801641+patrick-faulkner@users.noreply.github.com>
2019-12-02 21:08:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -292,7 +292,7 @@ app.post('/regPage/valid', function(req, res){
 
 // will render the search page
 app.get('/tutor-finder', function(req, res){
-  var query1 = 'SELECT id, firstname, lastname, rating, subjects, location FROM users ORDER BY lastname ASC;';
+  var query1 = "SELECT id, firstname, lastname, rating, subjects, location FROM users WHERE tutor = true AND location = '" + req.session.loc + "'  ORDER BY lastname ASC;";
   db.query(query1, task => {
       return task.batch([
           task.any(query1)
